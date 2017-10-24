c905329ae66fea00ecba0980828711f1a92421ba
Alexander Popiak <alexander.popiak@nexenio.com>
2017-10-24 14:25:56 +0200

    implement storage manager

--- src/lib/storage/storage_manager.cpp
+++ src/lib/storage/storage_manager.cpp
@@ -10,37 +10,47 @@
 namespace opossum {
 
 StorageManager& StorageManager::get() {
-  throw std::runtime_error("Implement StorageManager::get");
+  static StorageManager manager;
+  return manager;
 }
 
 void StorageManager::add_table(const std::string& name, std::shared_ptr<Table> table) {
-  // Implementation goes here
+  _tables.emplace(name, table);
 }
 
 void StorageManager::drop_table(const std::string& name) {
-  // Implementation goes here
+  if (has_table(name)) {
+    _tables.erase(name);
+  } else {
+    throw std::runtime_error(std::string("not table named: '") + name + "'");
+  }
 }
 
 std::shared_ptr<Table> StorageManager::get_table(const std::string& name) const {
-  // Implementation goes here
-  return nullptr;
+  return _tables.at(name);
 }
 
 bool StorageManager::has_table(const std::string& name) const {
-  // Implementation goes here
-  return false;
+  return _tables.count(name);
 }
 
 std::vector<std::string> StorageManager::table_names() const {
-  throw std::runtime_error("Implement StorageManager::table_names");
+  std::vector<std::string> keys;
+  keys.reserve(_tables.size());
+  std::transform(_tables.cbegin(), _tables.cend(), std::back_inserter(keys), [] (const auto &pair) { return pair.first; });
+  return keys;
 }
 
 void StorageManager::print(std::ostream& out) const {
-  // Implementation goes here
+  out << "StorageManager{ ";
+  for (const auto &name : table_names()) {
+    out << "'" << name << "' ";
+  }
+  out << "}";
 }
 
 void StorageManager::reset() {
-  // Implementation goes here;
+  get() = StorageManager();
 }
 
 }  // namespace opossum
