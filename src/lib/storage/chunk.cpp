164575e74f1126dba421a386f4bcc1c73bd75ee5
Alexander Popiak <alexander.popiak@nexenio.com>
2017-10-24 12:27:11 +0200

    clang format run

--- src/lib/storage/chunk.cpp
+++ src/lib/storage/chunk.cpp
@@ -14,25 +14,19 @@
 
 namespace opossum {
 
-void Chunk::add_column(std::shared_ptr<BaseColumn> column) {
-  _columns.push_back(column);
-}
+void Chunk::add_column(std::shared_ptr<BaseColumn> column) { _columns.push_back(column); }
 
 void Chunk::append(const std::vector<AllTypeVariant> values) {
   DebugAssert(values.size() == _columns.size(),
-    "Tried to append value vector with differing amount of columns to the chunk.");
+              "Tried to append value vector with differing amount of columns to the chunk.");
   for (uint16_t index = 0; index < values.size(); ++index) {
     _columns[index]->append(values[index]);
   }
 }
 
-std::shared_ptr<BaseColumn> Chunk::get_column(ColumnID column_id) const {
-  return _columns[column_id];
-}
+std::shared_ptr<BaseColumn> Chunk::get_column(ColumnID column_id) const { return _columns[column_id]; }
 
-uint16_t Chunk::col_count() const {
-  return _columns.size();
-}
+uint16_t Chunk::col_count() const { return _columns.size(); }
 
 uint32_t Chunk::size() const {
   if (!_columns.size()) {
