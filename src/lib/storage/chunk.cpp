06295a7236b3fffb5ef652f7f3be6af36dac284b
Alexander Popiak <alexander.popiak@nexenio.com>
2017-10-31 16:34:40 +0100

    add DebugAssert for get_column in Chunk

--- src/lib/storage/chunk.cpp
+++ src/lib/storage/chunk.cpp
@@ -27,7 +27,10 @@ void Chunk::append(const std::vector<AllTypeVariant> values) {
   }
 }
 
-std::shared_ptr<BaseColumn> Chunk::get_column(ColumnID column_id) const { return _columns[column_id]; }
+std::shared_ptr<BaseColumn> Chunk::get_column(ColumnID column_id) const {
+  DebugAssert(column_id < _columns.size(), "Chunk::get_column: column_id out of range: " + std::to_string(column_id));
+  return _columns[column_id];
+}
 
 uint16_t Chunk::col_count() const { return _columns.size(); }
 
