50ccfd3d802882b395b4214884b01694bd90e18b
Alexander Popiak <alexander.popiak@nexenio.com>
2017-10-31 17:19:44 +0100

    add correct print method to storage_manager

--- src/bin/playground.cpp
+++ src/bin/playground.cpp
@@ -2,6 +2,8 @@
 #include <memory>
 
 #include "../lib/storage/chunk.hpp"
+#include "../lib/storage/storage_manager.hpp"
+#include "../lib/storage/table.hpp"
 #include "../lib/storage/value_column.hpp"
 #include "../lib/utils/assert.hpp"
 
@@ -12,5 +14,13 @@ int main() {
   opossum::Chunk chunk;
   chunk.add_column(column);
   chunk.append({42});
+
+  auto table = std::make_shared<opossum::Table>();
+  table->add_column("values", "int");
+  table->append({3});
+  auto &man = opossum::StorageManager::get();
+  man.add_table("my table", table);
+  man.add_table("", table);
+  man.print(std::cout);
   return 0;
 }
