84416051801eaa0370dcc462bffa75f382ed4652
Soeren Tietboehl <soeren.tietboehl@student.hpi.de>
2017-11-07 12:11:07 +0100

    compile and test script can execute coverage
    usage: ./coverage_and_test.sh coverage

--- compile_and_test.sh
+++ compile_and_test.sh
@@ -2,8 +2,18 @@
 set -e
 build_dir="cmake-build-debug"
 target="hyriseTest"
+
+if [ $1 = "coverage" ]; then
+    target="hyriseCoverage"
+fi
+
 test_command=$target
 cd $build_dir
 make $target -j4
 cd ".."
-./$build_dir/$test_command
+
+if [ "$target" = "hyriseTest" ]; then
+    ./$build_dir/$test_command
+elif [ "$target" = "hyriseCoverage" ]; then
+    ./scripts/coverage.sh
+fi
